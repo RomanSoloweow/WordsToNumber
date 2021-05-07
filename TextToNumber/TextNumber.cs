964e7122027d9f6f34652020b19d6e76c7045ec0
GMIKE <roman.soloweow@gmail.com>
2021-05-07 03:50:48 +0300

    реализация словаря склонений и парсинга текста

--- TextToNumber/TextNumber.cs
+++ TextToNumber/TextNumber.cs
@@ -1,85 +1,149 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
 namespace TextToNumber
 {
     public class TextNumber
     {
-        private Dictionary<string, List<string>> MapForReplace { get; } = new()
+        private Dictionary<int, string> Declinations { get; } = new()
         {
-            { "два", new List<string>(){"две"}},
-            { "один", new List<string>(){"одна"}},
-            { "тысяча", new List<string>(){"тысяч([а-я])*"}},
-            { "миллион", new List<string>(){@"(миллион)([а-я])*"}},
-            { "миллиард", new List<string>(){@"(миллиард)([а-я])*"}},
-            { "минус", new List<string>(){"-"}}
+            {1, @"одн(ого|ому|им|ом|ой|ою|их|ими|им|и|а|о|у)"},
+            {2, @"дв(ух|умя|ум|а)"},
+            {3, @"тр(ех|емя|ем|и)"},
+            {4, @"четыр(ех|ем|ьмя|е)"},
+            {5, @"пят(и|ью|ь)"},
+            {6, @"шест(и|ью|ь)"},
+            {7, @"сем(и|ью|ь)"},
+            {8, @"вос(емью|ьми|емь)"},
+            {9, @"девят(и|ью|ь)"},
+            {10, @"десят(и|ью|ь)"},
+            {11, @"одиннадцат(и|ью|ь)"},
+            {12, @"двенадцат(и|ью|ь)"},
+            {13, @"тринадцат(и|ью|ь)"},
+            {14, @"четырнадцат(и|ью|ь)"},
+            {15, @"пятнадцат(и|ью|ь)"},
+            {16, @"шестнадцат(и|ью|ь)"},
+            {17, @"семнадцат(и|ью|ь)"},
+            {18, @"восемнадцат(и|ью|ь)"},
+            {19, @"девятнадцат(и|ью|ь)"},
+            {20, @"двадцат(и|ью|ь)"},
+            {30, @"тридцат(и|ью|ь)"},
+            {40, @"сорок(а)?"},
+            {50, @"пят(ьдесят|идесяти|ьюдесятью)"},
+            {60, @"шест(ьдесят|идесяти|ьюдесятью)"},
+            {70, @"сем(ьдесят|идесяти|ьюдесятью)"},
+            {80, @"вос(емьдесят|ьмидесяти|ьмьюдесятью)"},
+            {90, @"девяност(а|о)"},
+            {100, @"ст(а|о)"},
+            {200, @"дв(ести|ухсот|умстам|умястами|ухстах)"},
+            {300, @"тр(иста|ёхсот|ёмстам|емястами)"},
+            {400, @"четыр(еста|ёхсот|ёмстам|ьмястами|ёхстах)"},
+            {500, @"пят(ьсот|исот|истам|ьюстами|истах)"},
+            {600, @"шест(ьсот|исот|истам|ьюстами|истах)"},
+            {700, @"сем(ьсот|исот|истам|ьюстами|истах)"},
+            {800, @"вос(емьсот|ьмисот|ьмистам|емьсот|емьюстами|ьмистах)"},
+            {900, @"девят(ьсот|исот|истам|ьюстами|истах)"},
+            {1000, @"тысяч(ами|ей|а|и|е|у)?"},
+            {1000000, @"миллион(ами|а|у|ом|е|ов)?"},
+            // {1000000000, @"миллиард(ами|а|у|ом|е|ов)?"},
+            // {1000000000000, @"триллион(а|у|ом|ов)?"},
+            // {1000000000000000, @"миллиард(а|у|ом|ов)?"},
         };
-        private Dictionary<string, int> Units { get; } = new()
-        {
-            {"один", 1},
-            {"два", 2},
-            {"три", 3},
-            {"четыре", 4},
-            {"пять", 5},
-            {"шесть", 6},
-            {"семь", 7},
-            {"восемь", 8},
-            {"девять", 9},
-        };
-        private Dictionary<string, int> Dozen2 { get; } = new()
-        {
-            {"десять", 10},
-            {"одиннадцать", 11},
-            {"двенадцать", 12},
-            {"тринадцать", 13},
-            {"четырнадцать", 14},
-            {"пятнадцать", 15},
-            {"шестнадцать", 16},
-            {"семнадцать", 17},
-            {"восемнадцать", 18},
-            {"девятнадцать", 19}
-        };
-        
-        private Dictionary<string, int> Dozens { get; } = new()
-        {
-            {"двадцать", 20},
-            {"тридцать", 30},
-            {"сорок", 40},
-            {"пятьдесят", 50},
-            {"шестьдесят", 60},
-            {"семьдесят", 70},
-            {"восемьдесят", 80},
-            {"девяносто", 90},
-        };
-        
-        private Dictionary<string, int> Hundreds { get; } = new()
-        {
-            {"сто", 100},
-            {"двести", 100},
-            {"триста", 100},
-            {"четыреста", 100},
-            {"пятьсот", 100},
-            {"шестьсот", 100},
-            {"семьсот", 100},
-            {"восемьсот", 100},
-            {"девятьсот", 100},
-        };
-        
-        
-        public long Parse(string text)
+
+        private Regex NumberRegex = new Regex(@"\d+(\.\d+)?", RegexOptions.Compiled);
+
+
+        public string TextWordsToNumber(string text)
         {
             text = text.ToLower();
-            foreach (var (name, list) in MapForReplace)
+
+            foreach (var declination in Declinations.Reverse())
             {
-                foreach (var value in list)
+                text = Regex.Replace(text, declination.Value, declination.Key.ToString());
+            }
+            
+            var words = text.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> result = new();
+            List<string> buffer = new(); 
+            foreach (var word in words)
+            {
+                if (!NumberRegex.IsMatch(word))
                 {
-                    text = Regex.Replace(text, value, name);
+                    if (buffer.Count < 2)
+                    {
+                        result.AddRange(buffer);
+                        buffer.Clear();
+                    }
+                    else
+                    {
+                        result.Add(WordsToNumber(buffer).ToString());
+                        buffer.Clear();
+                    }
+                    
+                    result.Add(word);
+                }
+                else
+                {
+                    buffer.Add(word);
                 }
             }
-            var numbers = 
-       
-            return 0;
+
+            if (buffer.Count <2)
+            {
+                result.AddRange(buffer);
+                buffer.Clear();
+            }
+            else
+            {
+                result.Add(WordsToNumber(buffer).ToString());
+                buffer.Clear();
+            }
+            
+            return string.Join(" ", result);
+        }
+        
+        private int WordsToNumber(List<string> words)
+        {
+            var numbers = words.Select(int.Parse).ToArray();
+            int result = 0;
+            List<int> numbers2 = new ();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var temp = numbers[i];
+                while (i < numbers.Length-1 && numbers[i] > numbers[i + 1])
+                {
+                    temp+=numbers[i+1];
+                    i++;
+                }
+                
+                if (temp!=numbers[i])
+                {
+                  numbers2.Add(temp);
+                }
+                else
+                {
+                    numbers2.Add(numbers[i]); 
+                }
+            }
+            
+            for (int i = 0; i < numbers2.Count; i++)
+            {
+                if (i < numbers2.Count-1 && numbers2[i] < numbers2[i + 1])
+                {
+                    result += numbers2[i] * numbers2[i + 1];
+                    i++;
+                }
+                else
+                {
+                    result += numbers2[i];
+                }
+            }
+
+            return result;
         }
     }
 }
