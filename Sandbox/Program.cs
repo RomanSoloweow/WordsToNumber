65617d9072dc4ddca90a40f75c238b193200d6a1
GMIKE <roman.soloweow@gmail.com>
2021-05-07 03:50:54 +0300

    тестирование

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -9,7 +9,10 @@ namespace Sandbox
         {
             var parser = new TextNumber();
             
-            var t = parser.Parse("две миллионов");
+            var result = parser.TextWordsToNumber("пятьюстами пятью миллионами оплатил дом, который купил" +
+                                                  " на триста шестьдесят семь тысяч дешевле");
+            result = parser.TextWordsToNumber("семьдесят два года и ещё двести пять лет мучений");
+            result = parser.TextWordsToNumber("сто одиннадцать тысяч фиолетовых оленей");
             Console.WriteLine("Hello World!");
         }
     }
