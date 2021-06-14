7405c9c4a1bf0501ead3277d6dc7b145898269a3
GMIKE <roman.soloweow333@ya.ru>
2021-06-14 13:37:06 +0300

    Update travis-ci-build.sh

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -4,6 +4,6 @@ dotnet ./tools/sonar/SonarScanner.MSBuild.dll begin /o:"gmike" /k:"RomanSoloweow
 echo "Running build..."
 dotnet build WordsToNumber.sln
 echo "Running tests..."
-dotnet test WordsToNumber.Tests/WordsToNumber.Tests.csproj --collect "Code Coverage" --logger trx --results-directory "TestsResults"
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../lcov
 echo "Executing MSBuild DLL end command..."
 dotnet ./tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
