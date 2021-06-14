fd509a6a024d8f87d2b396fe586f4549e0265652
GMIKE <roman.soloweow333@ya.ru>
2021-06-14 11:44:41 +0300

    Update travis-ci-build.sh

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "Executing MSBuild DLL begin command..."
-dotnet tools/sonar/SonarScanner.MSBuild.dll begin /o:"GMIKE" /k:"RomanSoloweow_WordsToNumber" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
+dotnet ./tools/sonar/SonarScanner.MSBuild.dll begin /o:"GMIKE" /k:"RomanSoloweow_WordsToNumber" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
 dotnet build WordsToNumber.sln
 echo "Running tests..."
 dotnet test WordsToNumber.sln
 echo "Executing MSBuild DLL end command..."
-dotnet tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
+dotnet ./tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
